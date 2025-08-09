8672c44386d8ff05215348ef720374d31a418728
Luc1100 <52473174+Luc1100@users.noreply.github.com>
2025-08-09 18:00:24 -0400

    Add grid size changing functionality

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const gridContainer = document.querySelector('#grid-container');
 
-for (let i = 0; i < 16*16; i++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.setAttribute("class", "grid-square")
-    gridSquare.setAttribute("id", `grid-square-${i}`)
-    gridContainer.appendChild(gridSquare)
-    
-    gridSquare.addEventListener("mouseover", () => {
-        gridSquare.classList.add("on-hover")
-    });
+function setUpGrid(size) {
+    gridContainer.textContent = '';
+    for (let i = 0; i < size*size; i++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.setAttribute("class", "grid-square");
+        gridSquare.setAttribute("id", `grid-square-${i}`);
+        gridSquare.setAttribute("style", `width: ${1/size * 100}%`)
+        gridContainer.appendChild(gridSquare);
+        
+        gridSquare.addEventListener("mouseover", () => {
+            gridSquare.classList.add("on-hover");
+        });
+    }
 }
+
+const gridSizeBtn = document.querySelector("#grid-size-btn");
+gridSizeBtn.addEventListener("click", () => {
+    let size = prompt("Enter a grid size (1-100)");
+    while (!(size < 100)) {
+        size = prompt("Size must be 100 or less!");
+    }
+    setUpGrid(size)
+});
+
+setUpGrid(16)
