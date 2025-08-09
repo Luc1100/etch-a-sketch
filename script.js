46d5e87fe600fce5fefb8700663bfa46e2f90201
Luc1100 <52473174+Luc1100@users.noreply.github.com>
2025-08-09 17:48:39 -0400

    Add trail on mousehover

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ for (let i = 0; i < 16*16; i++) {
     gridSquare.setAttribute("class", "grid-square")
     gridSquare.setAttribute("id", `grid-square-${i}`)
     gridContainer.appendChild(gridSquare)
+    
+    gridSquare.addEventListener("mouseover", () => {
+        gridSquare.classList.add("on-hover")
+    });
 }
